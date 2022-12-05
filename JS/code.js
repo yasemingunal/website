b305e5822d6e906198d33292f1bf80065ed2b1e0
yasemingunal <76755092+yasemingunal@users.noreply.github.com>
2022-12-04 22:35:59 -0500

    Add files via upload

---
+++ JS/code.js
@@ -0,0 +1,23 @@
+var message = document.getElementById('contact-message');
+
+message.style.height="75px";
+
+function goResume(){
+    window.location="/Assets/Yasemin Gunal Resume - November 2022.pdf";
+}
+
+function goLinkedin(){
+    window.location="https://www.linkedin.com/in/yasemin-gunal/";
+}
+
+function goPoster(){
+    window.location="/assets/FinalPosterPDF.pdf";
+}
+
+function goEPLGitHub(){
+    window.location = "https://github.com/yasemingunal/premier_league_data_analysis";
+}
+
+function goGaleGitHub(){
+    window.location = "https://github.com/yasemingunal/ReachGaleProject";
+}
